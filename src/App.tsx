c39f5335cfd11003a9991fa4711e9f7466f4b4d3
Dmitriy <dmitryavgst@gmail.com>
2024-11-29 20:10:44 +0500

    -

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import React from "react";
+
 import { Main } from "components/Main/Main";
 
-export const App = () => <Main />;
+import { tg } from "constants/tg-api";
+
+export const App = () => {
+  React.useEffect(() => {
+    tg.ready();
+  }, []);
+
+  return <Main />;
+};
